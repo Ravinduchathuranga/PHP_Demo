b98cdc3249b3c7b7c6a07e2ac1be333ec812a947
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2024-05-16 09:49:34 +0530

    if ladder updated!

---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- if -->
+<?php
+
+$marks = 90;
+if($marks >= 90){
+    echo "A+";
+}
+else if($marks >= 80){
+    echo "A";
+}
+else if($marks >= 70){
+    echo "B";
+}
+else if($marks >= 60){
+    echo "C";
+}
+else{
+    echo "Fail";
+
+}
+
+?>
